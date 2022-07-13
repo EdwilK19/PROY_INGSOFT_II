46a33b919052283dc0d2db4046dce59e89344db2
Edwil K. Aguilar Gonzalez <70296542+EdwilK19@users.noreply.github.com>
2022-07-13 12:51:45 -0500

    Update espm_humedad.php
    Nueva modificación

--- DB_Conection/DB_FILEESP_CONECTION/espm_humedad.php
+++ DB_Conection/DB_FILEESP_CONECTION/espm_humedad.php
@@ -5,14 +5,14 @@ include'conexion.php';
 if ($con) {
     echo "Conexion con base de datos exitosa! ";
 
-    if(isset($_POST['valoruv'])) {
-        $valoruv = $_POST['valoruv'];
-        echo "Registro Rayos UV";
-        echo "Valor de Sensor: ".$valoruv;
+    if(isset($_POST['humedad'])) {
+        $humedad = $_POST['humedad'];
+        echo "Registro de humedad";
+        echo "Valor de Sensor: ".$humedad;
         date_default_timezone_set('america/bogota');
         $fecha = date("Y-m-d");
 
-        $consulta = "INSERT INTO marco(valorsensor,fecha) VALUES ('$valoruv', '$fecha')";
+        $consulta = "INSERT INTO mod_humedad(humedad,fecha) VALUES ('$humedad', '$fecha')";
        // $consulta = "UPDATE ESP32 SET ESpO2='$ESpO2',BMP='$BPM' WHERE Id = 1";
         $resultado = mysqli_query($con, $consulta);
         if ($resultado){
