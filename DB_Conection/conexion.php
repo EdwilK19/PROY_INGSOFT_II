64fd85a621689b7b7defc77665582ff2db3c2edf
Edwil K. Aguilar Gonzalez <70296542+EdwilK19@users.noreply.github.com>
2022-07-13 10:49:29 -0500

    Create conexion.php
    Archivo de Testeo de conexión con el servidor de la base de datos

---
+++ DB_Conection/conexion.php
@@ -0,0 +1,9 @@
+<?php
+
+        $user = "u4499_XdgTM1wEDo";
+        $pass = "HWS7SKHG1+Ck0dZutv6ygo6.";
+        $server = "";
+        $db ="s4499_DBPROYINGII";
+        $con = mysqli_connect($server, $user, $pass, $db);
+
+?>
