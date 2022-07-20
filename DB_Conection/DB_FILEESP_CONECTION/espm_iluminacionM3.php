bf91f9bbd87547850ab92795fe240f5b0aeced71
Edwil K. Aguilar Gonzalez <70296542+EdwilK19@users.noreply.github.com>
2022-07-20 16:40:59 -0500

    DB ACCESS_M3

---
+++ DB_Conection/DB_FILEESP_CONECTION/espm_iluminacionM3.php
@@ -0,0 +1,31 @@
+<?php
+
+include'conexion.php';
+
+if ($con) {
+    echo "Conexion con base de datos exitosa! ";
+
+    if(isset($_POST['iluminacionm3'])) {
+        $iluminacionm3 = $_POST['iluminacionm3'];
+        echo "Registro de Iluminación";
+        echo "Valor de Sensor: ".$iluminacionm3;
+        date_default_timezone_set('america/bogota');
+        $fecha = date("Y-m-d");
+
+        $consulta = "INSERT INTO mod_iluminacionm3(iluminacion,fecha) VALUES ('$iluminacion', '$fecha')";
+       // $consulta = "UPDATE ESP32 SET ESpO2='$ESpO2',BMP='$BPM' WHERE Id = 1";
+        $resultado = mysqli_query($con, $consulta);
+        if ($resultado){
+            echo " Registo en base de datos OK! ";
+        } else {
+            echo " Falla! Registro BD";
+        }
+    }
+
+
+} else {
+    echo "Falla! conexion con Base de datos ";
+}
+
+
+?>
--- Modulo_Iluminacion/ModuloIluminacion.ino
+++ Modulo_Iluminacion/ModuloIluminacion.ino
@@ -1,3 +1,12 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+
+// Se definen las credenciales de red
+const char* ssid ="ESP32";
+const char* password ="12345678";
+WiFiClient client;
+
 int ledPin = 2; // Pin de referencia al led en la esp8266
 
 int ldrPin = 0; // LDR en el pin analogico 0
@@ -6,17 +15,26 @@ int ldrPin = 0; // LDR en el pin analogico 0
 
 int ldrValue = 0; //inicializacion de la variable que almacenara la lectura
 
- 
+
 
 void setup() {
 
   Serial.begin(9600);
-
   pinMode(ledPin,OUTPUT);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando...");
+  while (WiFi.status()!= WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Conexión OK!");
+  Serial.print("IP Local: ");
+  Serial.println(WiFi.localIP());
 
 }
 
- 
+
 
 void loop() {
 
@@ -25,7 +43,7 @@ void loop() {
   Serial.println(ldrValue); //se imprime el valor en monitor
 
   // condición para encender el led dependiendo de la intensidad de luz leída
-  
+
   if (ldrValue >= 100){
 
     digitalWrite(ledPin,HIGH);
@@ -40,4 +58,34 @@ void loop() {
 
   delay(500);
 
+}
+
+void EnvioDatos(){
+  if (WiFi.status() == WL_CONNECTED){
+     HTTPClient http;  //se crea el objeto http
+     String datos_a_enviar = "iluminacionm3=" + String(ldrValue);
+
+     http.begin(client,"http://ggsxcloud.website/espm_iluminacionM3.php");
+     http.addHeader("Content-Type", "application/x-www-form-urlencoded"); // defino texto plano..
+
+     int codigo_respuesta = http.POST(datos_a_enviar);
+
+     if (codigo_respuesta>0){
+      Serial.println("Código HTTP: "+ String(codigo_respuesta));
+        if (codigo_respuesta == 200){
+          String cuerpo_respuesta = http.getString();
+          Serial.println("El servidor respondió: ");
+          Serial.println(cuerpo_respuesta);
+        }
+     } else {
+        Serial.print("Error enviado POST, código: ");
+        Serial.println(codigo_respuesta);
+     }
+
+       http.end();  //se libera recursos recursos
+
+  } else {
+     Serial.println("Error en la conexion WIFI");
+  }
+  delay(3000); //espera 3s
 }
